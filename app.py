041694840c00755f47178154476acaec762c5c84
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2025-09-23 20:34:55 +0100

    added CORS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from bot import get_bot_response
+sfrom flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, origins=["http://localhost:3000"])
 
 @app.route("/chat", methods=["POST"])
 def chat():
